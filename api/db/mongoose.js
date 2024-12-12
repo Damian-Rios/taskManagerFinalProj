544b499fb0462e8b234edb2166e802e6284c4bc7
Damian-Rios <102697604+Damian-Rios@users.noreply.github.com>
2024-12-12 16:07:26 -0600

    Update mongoose.js

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TaskManager').then(() => {
+mongoose.connect('mongodb://127.0.0.1:27017/TaskManager').then(() => {
     console.log("Connected to MongoDB successfully :)");
 }).catch((e) => {
     console.log("Error while attempting to connect to MongoDB");
